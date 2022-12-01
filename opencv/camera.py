c87fe505e3ea45bcc4f34b46a970b3bfaf83af4d
nhandothanh01 <nhandothanh01@gmail.com>
2022-12-01 12:06:22 +0900

    Update camera.py

--- opencv/camera.py
+++ opencv/camera.py
@@ -7,7 +7,10 @@ cam = cv2.VideoCapture(0)
 ret, frame = cam.read()
 
 # 写真を表示する
-cv2.imshow('Camera', frame)
+cv2.imshow('camera', frame)
+
+# 写真を保存する
+cv2.imwrite('aloha.jpg', frame)
 
 # おわる
 key = cv2.waitKey()		# キーを押すまで待つ
